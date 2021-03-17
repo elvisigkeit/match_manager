8497af6069db291d61e70b2ce6a9fd4ebe4ca7d5
Elvis Nobrega <elvismdnin@gmail.com>
2021-03-17 18:46:15 -0300

    Includes the creation logic of chess table

---
+++ internal/data/table.go
@@ -0,0 +1,51 @@
+package data
+
+type Square struct {
+    Color   rune    `json:"color"`
+    Piece   rune   `json:"piece"`
+}
+
+func CreateTable() [8][8]Square {
+    var finalSquare [8][8]Square
+    for indx := 0; indx < 8; indx++ {
+        for indy := 0; indy < 8; indy++ {
+            if indy > 1 && indy < 6 {
+                continue
+            }
+            if indy == 1 {
+                finalSquare[indx][indy].Piece = 'P'
+                finalSquare[indx][indy].Color = 'W'
+                continue
+            }
+            if indy == 6 {
+                finalSquare[indx][indy].Piece = 'P'
+                finalSquare[indx][indy].Color = 'B'
+                continue
+            }
+            if indy == 0 || indy == 7 {
+                if indy == 0 {
+                    finalSquare[indx][indy].Color = 'W'
+                } else {
+                    finalSquare[indx][indy].Color = 'B'
+                }
+
+                if indx == 0 || indx == 7 {
+                    finalSquare[indx][indy].Piece = 'R'
+                }
+                if indx == 1 || indx == 6 {
+                    finalSquare[indx][indy].Piece = 'N'
+                }
+                if indx == 2 || indx == 5 {
+                    finalSquare[indx][indy].Piece = 'B'
+                }
+                if indx == 3 {
+                    finalSquare[indx][indy].Piece = 'Q'
+                }
+                if indx == 4 {
+                    finalSquare[indx][indy].Piece = 'K'
+                }
+            }
+        }
+    }
+    return finalSquare
+}
---
+++ test/table_test.go
@@ -0,0 +1,34 @@
+package web
+
+import (
+	"github.com/elvismdnin/crius/internal/data"
+	
+	"fmt"
+	"testing"
+)
+
+func TestCreateTable(t *testing.T) {
+	table := data.CreateTable()
+	var strTable string
+	for _, column := range(table) {
+		for _, val := range(column) {
+			if val.Piece != 0 {
+				square := fmt.Sprintf("%c%c ", val.Color,val.Piece)
+				strTable += square
+			} else {
+				strTable += "   "
+			}
+		}
+	}
+	
+	if strTable != 	"WR WP             BP BR "+
+					"WN WP             BP BN "+
+					"WB WP             BP BB "+
+					"WQ WP             BP BQ "+
+					"WK WP             BP BK "+
+					"WB WP             BP BB "+
+					"WN WP             BP BN "+
+					"WR WP             BP BR "{
+		t.Error("The table being created is wrong")
+	}
+}
