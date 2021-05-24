d155c0777913c356c95ae13cc1a81e3e65f4a93d
Elvis Nobrega <elvismdnin@gmail.com>
2021-05-24 15:34:31 -0300

    Adds messages of log, for debugging

--- web/command_api.go
+++ web/command_api.go
@@ -6,6 +6,7 @@ import (
 	"github.com/elvismdnin/match_manager/internal/data"
 	"github.com/elvismdnin/match_manager/internal/web"
 	"net/http"
+	"log"
 
 	"github.com/gorilla/mux"
 )
@@ -16,17 +17,20 @@ func CommandMove() web.Handler {
 			r.Path("/move").Methods("POST")
 		},
 		Func: func(w http.ResponseWriter, r *http.Request) {
+			log.Println("Moving piece")
 			var move data.Move
 			buf := new(bytes.Buffer)
 			_, _ = buf.ReadFrom(r.Body)
 			body := buf.String()
 			decodeErr := json.Unmarshal([]byte(body), &move)
 			if decodeErr != nil {
+				log.Println("Invalid move json")
 				http.Error(w, decodeErr.Error(), http.StatusBadRequest)
 				return
 			}
 
 			if !data.CheckValidMovement(move.OriX, move.OriY, move.DesX, move.DesY, data.Table) {
+				log.Println("Invalid movement")
 				http.Error(w, "Invalid movement", http.StatusBadRequest)
 				return
 			}
@@ -36,8 +40,10 @@ func CommandMove() web.Handler {
 
 			encodeErr := json.NewEncoder(w).Encode(data.Table)
 			if encodeErr != nil {
+				log.Println("Problem with enconding")
 				http.Error(w, encodeErr.Error(), http.StatusInternalServerError)
 			}
+			log.Println("Success on moving")
 		},
 	}
 }
