e8797a58f5fde1356f8c2c5164c9582da6d18469
Elvis Nobrega <elvismdnin@gmail.com>
2021-05-24 15:36:12 -0300

    Removes old code commented

--- web/info_api.go
+++ web/info_api.go
@@ -19,22 +19,3 @@ func GetTable(table [8][8]data.Square) web.Handler {
         },
     }
 }
-
-//
-//func GetPosition(players map[string]data.Player) web.Handler {
-//    return web.Handler{
-//        Route: func(r *mux.Route) {
-//            r.Path("/position/{id}").Methods("GET")
-//        },
-//		Func: func(w http.ResponseWriter, r *http.Request) {
-//            params := mux.Vars(r)
-//            if player, ok := players[params["id"]]; ok {
-//                pos := player.Pos
-//                json.NewEncoder(w).Encode(pos)
-//            } else {
-//                log.Printf("ID doesn't correspond to a player\n")
-//                http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-//            }
-//        },
-//    }
-//}
